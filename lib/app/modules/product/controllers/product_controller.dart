9d116b4dd33350ae73406b752e01c933dff3c768
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2025-01-25 22:43:17 +0600

    check out button added

--- lib/app/modules/product/controllers/product_controller.dart
+++ lib/app/modules/product/controllers/product_controller.dart
@@ -8,10 +8,12 @@ class ProductController extends GetxController {
   @override
   void onInit() {
     products.addAll([
-      ProductModel(quantity: 1.obs, name: 'product 1', price: 12.34),
-      ProductModel(quantity: 1.obs, name: 'product 2', price: 15.76),
-      ProductModel(quantity: 1.obs, name: 'product 3', price: 16.56),
-      ProductModel(quantity: 1.obs, name: 'product 4', price: 11.59),
+      ProductModel(quantity: 0.obs, name: 'product 1', price: 12.34),
+      ProductModel(quantity: 0.obs, name: 'product 2', price: 15.76),
+      ProductModel(quantity: 0.obs, name: 'product 3', price: 16.56),
+      ProductModel(quantity: 0.obs, name: 'product 4', price: 11.59),
+      ProductModel(quantity: 0.obs, name: 'product 5', price: 14.59),
+      ProductModel(quantity: 0.obs, name: 'product 6', price: 11.59),
     ]);
 
     super.onInit();
--- lib/app/modules/product/views/product_view.dart
+++ lib/app/modules/product/views/product_view.dart
@@ -21,78 +21,103 @@ class ProductView extends StatelessWidget {
                   Get.toNamed('/cart');
                 })
           ]),
-      body: Obx(() {
-        return ListView.builder(
-            itemCount: controller.products.length,
-            itemBuilder: (context, index) {
-              final product = controller.products[index];
-              SizedBox(height: 30);
-              return Container(
-                margin: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: controller.getBackgroundColor(index),
-                  borderRadius: BorderRadius.circular(8), // Rounded corners
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withValues(alpha: 0.5), // Shadow color
-                      spreadRadius: 1,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3), // Shadow position
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: ListTile(
-                        title: Text(product.name),
-                        subtitle: Text('\$${product.price}'),
-                        onTap: () {
-                          Get.toNamed('/product-details', arguments: product);
-                        },
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(right: 10),
-                      child: Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {
-                                if (product.quantity > 0) {
-                                  product.quantity.value =
-                                      (product.quantity - 1).value;
-                                } else {
-                                  product.quantity.value = 0;
-                                }
-                              },
-                              icon: Icon(Icons.remove)),
-                          Obx(() {
-                            return Container(
-                              width: 45,
-                              height: 35,
-                              alignment: Alignment.center,
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(6)),
-                              child: Text('${product.quantity}'),
-                            );
-                          }),
-                          IconButton(
-                              onPressed: () {
-                                product.quantity.value =
-                                    (product.quantity + 1).value;
-                              },
-                              icon: Icon(Icons.add)),
+      body: Column(
+        children: [
+          Expanded(
+            child: Obx(() {
+              return ListView.builder(
+                  itemCount: controller.products.length,
+                  itemBuilder: (context, index) {
+                    final product = controller.products[index];
+                    SizedBox(height: 30);
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 7, horizontal: 15),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: controller.getBackgroundColor(index),
+                        borderRadius:
+                            BorderRadius.circular(8), // Rounded corners
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey
+                                .withValues(alpha: 0.5), // Shadow color
+                            spreadRadius: 1,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3), // Shadow position
+                          ),
                         ],
                       ),
-                    ),
-                  ],
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: ListTile(
+                              title: Text(product.name),
+                              subtitle: Text('\$${product.price}'),
+                              onTap: () {
+                                Get.toNamed('/product-details',
+                                    arguments: product);
+                              },
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () {
+                                      if (product.quantity > 0) {
+                                        product.quantity.value =
+                                            (product.quantity - 1).value;
+                                      } else {
+                                        product.quantity.value = 0;
+                                      }
+                                    },
+                                    icon: Icon(Icons.remove)),
+                                Obx(() {
+                                  return Container(
+                                    width: 45,
+                                    height: 35,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(6)),
+                                    child: Text('${product.quantity}'),
+                                  );
+                                }),
+                                IconButton(
+                                    onPressed: () {
+                                      product.quantity.value =
+                                          (product.quantity + 1).value;
+                                    },
+                                    icon: Icon(Icons.add)),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            }),
+          ),
+          Container(
+              height: 55,
+              width: 450,
+              margin: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(8)),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    elevation: 0, backgroundColor: Colors.transparent),
+                onPressed: () {},
+                child: Text(
+                  'Check Out',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
                 ),
-              );
-            });
-      }),
+              ))
+        ],
+      ),
     );
   }
 }
